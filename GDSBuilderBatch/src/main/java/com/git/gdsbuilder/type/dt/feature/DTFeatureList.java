50936e40facf99626c2a031970a30d7b54a70eb5
seulgilee <ghre55@git.co.kr>
2018-08-17 16:35:51 +0900

    OpenGDSValidatorBatch Update

--- GDSBuilderBatch/src/main/java/com/git/gdsbuilder/type/dt/feature/DTFeatureList.java
+++
@@ -1,9 +0,0 @@
-package com.git.gdsbuilder.type.dt.feature;
-
-import java.util.ArrayList;
-
-import org.opengis.feature.simple.SimpleFeature;
-
-public class DTFeatureList extends ArrayList<DTFeature> {
-
-}
